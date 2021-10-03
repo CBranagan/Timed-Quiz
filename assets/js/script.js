a059ef16f4f22eb604ab6106912a84ef9d159b46
Chad Branagan <chad.branagan@gmail.com>
2021-10-03 13:14:21 -0700

    start game logic, when you press start a question appears and the timer starts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 // console.log("hello");
 var formButton = document.getElementById('Btn');
 var timerEl = document.getElementById('Timer');
-
+var questionBox = document.getElementById('Question-box');
+var gameRules = document.getElementById('Game-rules');
 
 //when start button is clicked the timer starts running and the first question appears
 
@@ -9,9 +10,43 @@ var timerEl = document.getElementById('Timer');
 var startQuiz = function(event) {
     event.preventDefault();
     
+    createQuestonForm();
 
     countdown();
 }
+
+function createQuestonForm() {
+
+    var questionForm1 = document.createElement('div');
+    var question1 = document.createElement('form');
+    question1.textContent = "Question Form";
+    
+    var answer1 = document.createElement('input');
+    answer1.setAttribute("type", "radio");
+    
+    var answer2 = document.createElement('input')
+    answer2.setAttribute("type", "radio")
+    
+    var answer3 = document.createElement('input')
+    answer3.setAttribute("type", "radio")
+    
+    var answer4 = document.createElement('input')
+    answer4.setAttribute("type", "radio")
+    
+    gameRules.textContent = "";
+
+
+    question1.appendChild(answer1)
+    question1.appendChild(answer2)
+    question1.appendChild(answer3)
+    question1.appendChild(answer4)
+    questionBox.appendChild(gameRules);
+    questionBox.appendChild(question1);
+    questionBox.appendChild(questionForm1);
+    }
+
+
+//function to start timer
 
 function countdown() {
 
@@ -26,7 +61,6 @@ function countdown() {
             //endgame                       
         }
     }, 1000)
-    console.log(timeLeft)
 };
 
 
@@ -46,4 +80,4 @@ formButton.addEventListener("click", startQuiz);
 
 
 
-//when the game is over I can save my initials.
+//when the game is over I can save my initials and score.
