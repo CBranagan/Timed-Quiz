5dcaafc2901a17a1c93e044f0a75c092ec025159
Chad Branagan <chad.branagan@gmail.com>
2021-10-03 19:49:29 -0700

    created question form with radio buttons and a text field, will be used for object data later to loop through questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,39 +10,69 @@ var gameRules = document.getElementById('Game-rules');
 var startQuiz = function(event) {
     event.preventDefault();
     
-    createQuestonForm();
+    createQuestionForm();
 
     countdown();
 }
 
-function createQuestonForm() {
+function createQuestionForm() {
 
     var questionForm1 = document.createElement('div');
     var question1 = document.createElement('form');
     question1.textContent = "Question Form";
     
+    var answer1Box = document.createElement('div');
     var answer1 = document.createElement('input');
     answer1.setAttribute("type", "radio");
+    var answer1Text = document.createElement('span');
+    answer1Text.textContent = " answer1";
     
-    var answer2 = document.createElement('input')
-    answer2.setAttribute("type", "radio")
     
-    var answer3 = document.createElement('input')
-    answer3.setAttribute("type", "radio")
     
-    var answer4 = document.createElement('input')
-    answer4.setAttribute("type", "radio")
+    var answer2Box = document.createElement('div');
+    var answer2 = document.createElement('input');
+    answer2.setAttribute("type", "radio");
+    var answer2Text = document.createElement('span');
+    answer2Text.textContent = " answer2";
+    
+    var answer3Box = document.createElement('div');
+    var answer3 = document.createElement('input');
+    answer3.setAttribute("type", "radio");
+    var answer3Text = document.createElement('span');
+    answer3Text.textContent = " answer3";
+    
+    var answer4Box = document.createElement('div');
+    var answer4 = document.createElement('input');
+    answer4.setAttribute("type", "radio");
+    var answer4Text = document.createElement('span');
+    answer4Text.textContent = " answer4";
     
     gameRules.textContent = "";
 
+    
+    answer1Box.appendChild(answer1)
+    answer1Box.appendChild(answer1Text)
+    
+    answer2Box.appendChild(answer2)
+    answer2Box.appendChild(answer2Text)
+    
+    answer3Box.appendChild(answer3)
+    answer3Box.appendChild(answer3Text)
 
-    question1.appendChild(answer1)
-    question1.appendChild(answer2)
-    question1.appendChild(answer3)
-    question1.appendChild(answer4)
+    answer4Box.appendChild(answer4)
+    answer4Box.appendChild(answer4Text)
+    
+    
     questionBox.appendChild(gameRules);
     questionBox.appendChild(question1);
+
     questionBox.appendChild(questionForm1);
+    questionBox.appendChild(answer1Box)
+    questionBox.appendChild(answer2Box)
+    questionBox.appendChild(answer3Box)
+    questionBox.appendChild(answer4Box)
+
+
     }
 
 
